9456189dd5dce1e55d5ea6393b1b57d1ac151e74
Jaimy Magalhaes Coelho <mokin009x@hotmail.nl>
2018-02-28 15:49:22 +0100

    playfield is done and can be used to place cards

---
+++ UnityProject/Cardgaym/Assets/Hand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public Transform ExplenationCard;
+    public GameObject[] HandPosObj;
+    public List<Vector3> HandPos;
+    public string Owner;
+	// Use this for initialization
+	void Start ()
+	{
+	    for (int i = 0; i < HandPosObj.Length; i++)
+	    {
+	        HandPos.Add(HandPosObj[i].transform.position);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
