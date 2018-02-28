39c0cf1e5f3be89d62a2473382117cbaa7cb332a
Stan Vogels <stanvogels@live.nl>
2018-02-28 15:17:59 +0100

    I've done some adding to the project

---
+++ Assets/Classes/MClass/World.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World{
+
+    GameObject myWorld;
+    int mySize;
+
+    public World(GameObject WorldPrefab, int size)
+    {
+        myWorld = WorldPrefab;
+        mySize = size;
+        CreateWorld();
+    }
+
+    public void CreateWorld()
+    {
+        GameObject thisWorld = MonoBehaviour.Instantiate(myWorld);
+        thisWorld.transform.localScale = new Vector3(mySize, 0, mySize);
+    }
+
+}
---
+++ Assets/MScripts/MyGameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour {
+
+	World myWorld;
+    public GameObject worldPrefab;
+
+    private void Start()
+    {
+        myWorld = new World(worldPrefab, 100);
+    }
+
+}
