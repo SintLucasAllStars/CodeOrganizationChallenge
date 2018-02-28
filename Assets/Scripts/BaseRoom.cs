fad05d03ea9f40d146b8007658a6b5077cfe93c7
Martynas Gestautas <martgest@gmail.com>
2018-02-28 14:05:17 +0200

    Testing of json capabilities for saving settings

---
+++ Assets/Scripts/BaseRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRoom : MonoBehaviour {
+    
+    void Start() {
+        // Testing out json capabilites
+        Settings test = new Settings();
+        test.completedPuzzles.Add("puzzle1");
+        test.completedPuzzles.Add("puzzle2");
+        Debug.Log(JsonUtility.ToJson(test));
+    }
+
+}
