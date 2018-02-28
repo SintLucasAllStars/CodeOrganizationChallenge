b821d7c34f0fdd76d313d621df8207c803e03e7f
Max Neijndorff <m.neijndorff1999@gmail.com>
2018-02-28 12:40:40 +0100

    files in map gezet

---
+++ Assets/Brains.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brains : MonoBehaviour {
+    public List<Cards> cardsList;
+
+    int handLenght = 7;
+    int totalPlayer = 2;
+    int playerOneHealt;
+    int playerTwoHealt;
+
+
+    public void Start()
+    {
+        cardsList = new List<Cards>();
+        playerOneHealt = 30;
+        playerTwoHealt = 30;
+        Banana();
+        
+    }
+
+    public void Banana()
+    {
+        for (int i = 0; i < handLenght; i++)
+        {
+            int val = Random.Range(0, 2);
+            Cards.CardType t = Cards.CardType.Minion;
+
+            switch(val)
+            {
+                case 0: t = Cards.CardType.Minion;
+                    break;
+                case 1: t = Cards.CardType.Spell;
+                    break;
+            }
+            Debug.Log(t);
+            cardsList.Add(new Cards(t, Random.Range(1, 11), Random.Range(1, 16)));
+        }
+    }
+}
---
+++ Assets/Cards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Cards{
+    public enum CardType {Minion, Spell};
+
+    bool active;
+    CardType type;
+    int health;
+    int damage;
+    Sprite cardSprite;
+    GameObject gameObject;
+
+    public Cards(CardType cardType, int health, int damage)
+    {
+        active = true;
+    }
+
+    public void GetCard()
+    {
+        if (active)
+        {
+
+        }
+    }
+}
