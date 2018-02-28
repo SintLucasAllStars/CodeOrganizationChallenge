ae4282881d8224798b5d3d30a0fdf1e404d8eaff
sneekjs <sneekjs@users.noreply.github.com>
2018-02-28 13:53:10 +0100

    FoodClass and Test Script

--- Assets/Scripts/Classes/Food.cs
+++ Assets/Scripts/Classes/Food.cs
@@ -3,6 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Food {
-    public enum FoodType { };
 
+    public string[] foodType = new string[]{ "Tree", "Melon", "Bush"};
+    public string thisFoodType;
+
+    public float restoreValue;
+
+    public Food()
+    {
+        thisFoodType = foodType[Random.Range(0, foodType.Length)].ToString();
+        restoreValue = Random.Range(25, 75);
+    }
+
+    public Food(string currentFood)
+    {
+        thisFoodType = currentFood;
+
+        if (currentFood == "Tree")
+        {
+            this.restoreValue = Random.Range(55, 75);
+        }
+
+        if (currentFood == "Melon")
+        {
+            this.restoreValue = Random.Range(40, 55);
+        }
+
+        if (currentFood == "Bush")
+        {
+            this.restoreValue = Random.Range(25, 40);
+        }
+    }
 }
