e74a60456c7cbd9e76608acc7be9bfecf6ae387b
sneekjs <sneekjs@users.noreply.github.com>
2018-02-28 12:35:05 +0100

    Beginning of class creation

---
+++ Assets/Scripts/Classes/WorldManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+    public List<Creature> creatures = new List<Creature>();
+    public List<Food> foods = new List<Food>();
+
+    public enum Climate { Winter, Summer, Spring, Autumn};
+    public Climate worldClimate;
+
+    public Vector3 worldSize;
+
+    public int obstacleAmount;
+    public int foodAmount;
+
+    public float worldAge;
+}
