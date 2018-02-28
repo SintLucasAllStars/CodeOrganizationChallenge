ac078c795295c52cb6f28342741dbf1620af2c62
Martynas Gestautas <martgest@gmail.com>
2018-02-28 15:38:30 +0200

    Remake of main script

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+    public List<string> completedPuzzles;
+
+    Vector3 playerPosition;
+    Quaternion playerRotation;
+
+    public Vector3 startingPosition, startingRotation;
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        completedPuzzles = new List<string>();
+        playerPosition = startingPosition;
+        playerRotation = Quaternion.Euler(startingRotation);
+    }
+        
+}
