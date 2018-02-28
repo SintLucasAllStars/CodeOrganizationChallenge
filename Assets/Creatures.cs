a37eeaa52b504f941a30fff5bb77feff0e0036e5
Bulletdrone <Sam>
2018-02-28 15:24:34 +0100

    removed the nothing file in the test folder

---
+++ Assets/Creatures.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creatures : MonoBehaviour {
+	enum SocialStatus {Friend, Neutral, Enemy};
+	enum CombatBehaviour {Aggressive, Defensive, Coward};
+	enum EatingBehaviour {omnivore, carnivore, herbivore};
+
+	float speed;
+	float size;
+	float fertility;
+	float attractionRate;
+	float hunger;
+	float age, maxAge;
+	float combatEfficiency;
+}
--- Assets/Humans.cs
+++ Assets/Humans.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nothing : MonoBehaviour {
+public class Humans : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
