3f2c4ca194857d80b173fa5ba7a7d251b3ea7991
Daan Dekkers <32635080+DaanDekkers@users.noreply.github.com>
2018-03-01 20:46:23 +0100

    for some reason moet ik heel de game commiten :p

---
+++ Assets/Scripts/MouseLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour {
+
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+     //   if (Input.GetKeyDown(KeyCode.A))
+     //   {
+            Cursor.lockState = CursorLockMode.Locked;
+        //    Cursor.lockState = CursorLockMode.None;  
+      //  }
+    }
+}
