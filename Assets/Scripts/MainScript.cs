210398e0bfa7bacd42c4386ed10810bda15586d6
Martynas Gestautas <martgest@gmail.com>
2018-03-01 11:06:58 +0200

    Minor updates

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScript : MonoBehaviour {
 
-    public List<string> completedPuzzles;
+    public List<string> completedPuzzles;               // Completed puzzles list.
+    public GameObject player;                           // The player object (might be replaced).
+    public Vector3 startingPosition, startingRotation;  // Starting position and rotation of the player - set in editor.
 
-    Vector3 playerPosition;
-    Quaternion playerRotation;
+    Vector3 playerPosition;     /* Player position and rotation to be  */
+    Quaternion playerRotation;  /*   saved when loading a new scene.   */
 
-    public Vector3 startingPosition, startingRotation;
-
+    // Replace with singleton - Kars' job btw
     void Awake() {
         DontDestroyOnLoad(transform.gameObject);
 
@@ -20,10 +22,35 @@ public class MainScript : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Creates an empty completed puzzles list and sets player starting position
+    /// </summary>
     void Start() {
         completedPuzzles = new List<string>();
         playerPosition = startingPosition;
         playerRotation = Quaternion.Euler(startingRotation);
     }
-        
+
+    /// <summary>
+    /// Loads a puzzle's scene and saves players position
+    /// </summary>
+    /// <param name="puzzleSceneName">Puzzle scene name</param>
+    public void LoadPuzzle(string puzzleSceneName) {
+        playerPosition = player.transform.position;
+        playerRotation = player.transform.rotation;
+        SceneManager.LoadScene(puzzleSceneName, LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// Called when puzzle is completed, adds the puzzle's name into the completed
+    /// puzzles list, loads the main scene and restores the players position
+    /// </summary>
+    /// <param name="puzzleName">Puzzle name.</param>
+    public void FinishPuzzle(string puzzleName) {
+        completedPuzzles.Add(puzzleName);
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+        // Should theoretically work, but player might not exist yet? NEEDS TESTING
+        player.transform.SetPositionAndRotation(playerPosition, playerRotation);
+    }
 }
