a7b9f883bbb36fd88a8cec6c41220911a4d2dd22
sneekjs <sneekjs@users.noreply.github.com>
2018-03-02 17:08:42 +0100

    fixed the obstacle debug collision
    one downside is that the creatures dont have a rigidbody because of a bugg

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	void Start () {
+        StartCoroutine(MakeInDestructible());
+	}
+
+    public IEnumerator MakeInDestructible()
+    {
+        yield return new WaitForSeconds(1f);
+        this.gameObject.tag = "InDestructible";
+    }
+}
