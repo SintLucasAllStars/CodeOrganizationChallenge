84774a04c5c6b8d145ec2ba7543a42e9c47e3dff
Kars van den Eijnde <aofbliz@hotmail.com>
2018-03-02 20:10:44 +0100

    Added/Finished Test Puzzle

---
+++ Assets/Scripts/TestPuzzleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPuzzleScript : MonoBehaviour {
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("pressed");
+        
+            GameObject.Find("MainController").GetComponent<MainScript>().FinishPuzzle("test");
+        }
+
+        if (Input.anyKeyDown && !Input.GetMouseButtonDown(0))
+        {
+
+            Application.Quit();
+        }
+	}
+}
