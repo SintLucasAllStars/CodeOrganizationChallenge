89019786c058133938cacdadce5865fec266577d
craftercis <c.donkers@sintlucasedu.nl>
2018-03-02 19:40:45 +0100

    Finall

--- Assets/Scripts/Brains.cs
+++ Assets/Scripts/Brains.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class Brains : MonoBehaviour {
 
     List<Puzzle> puzzles;
+    List<Room> rooms;
 
     int currentpuzzle = -1;
+    int currentroom = 1;
 
     public InputField answerField;
     public Text feedback;
@@ -19,10 +21,10 @@ public class Brains : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        correct = false;
         puzzles = new List<Puzzle>();
+        rooms = new List<Room>();
         CreatePuzzle();
-
+        CreateRooms();
         nextPuzzle();
     }
 	
@@ -34,6 +36,7 @@ public class Brains : MonoBehaviour {
             feedback.text = "Good job!";
             nextPuzzle();
         }
+
     }
 
     public void nextPuzzle()
@@ -44,20 +47,67 @@ public class Brains : MonoBehaviour {
             questionText.text = puzzles[currentpuzzle].question;
             answerField.text = "";
         }
+        else
+        {
+            SceneManager.LoadScene("Main");
+            nextRoom();
+        }
+    }
+
+    public void nextRoom()
+    {
+        currentroom++;
+        if(currentroom < rooms.Count)
+        {
+            //SceneManager.LoadScene("Room2");
+            SceneManager.LoadScene(rooms[currentroom].name);
+            currentpuzzle = -1;
+        }
     }
 
     public void CreatePuzzle()
     {
-        puzzles.Add(new Puzzle("What is 5 x 5?", "25"));
-        puzzles.Add(new Puzzle("What is 6 + 1?", "7"));
-        puzzles.Add(new Puzzle("What is 5 - 4?", "1"));
+        if (currentroom == 1)
+        {
+            puzzles.Add(new Puzzle("What is 5 x 5?", "25"));
+            puzzles.Add(new Puzzle("What is 6 + 1?", "7"));
+            puzzles.Add(new Puzzle("What is 5 - 4?", "1"));
+        }
+
+        if (currentroom == 2)
+        {
+            puzzles.Add(new Puzzle("What is 2 + 10?", "12"));
+            puzzles.Add(new Puzzle("What is 2 x 7?", "14"));
+            puzzles.Add(new Puzzle("What is 12 - 4?", "8"));
+        }
     }
 
-    public void NextQuestion()
+    public void CreateRooms()
     {
-        if (correct == true)
+        if (currentroom == 2)
         {
-            SceneManager.LoadScene("Question_2");
+            rooms.Add(new Room("Room2"));
         }
+
+        if (currentroom == 3)
+        {
+            rooms.Add(new Room("Room3"));
+        }
+
+        if (currentroom == 4)
+        {
+            rooms.Add(new Room("Room4"));
+        }
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("Room1");
+    }
+
+    public void Next()
+    {
+        //SceneManager.LoadScene(rooms[currentroom].name);
+        nextRoom();
     }
 }
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Room {
+
+    public string name;
+
+    public Room(string name)
+    {
+        this.name = name;
+    }
+
+}
