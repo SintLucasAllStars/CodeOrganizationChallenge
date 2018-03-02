6ca79f7ea42f29b73ce14907a59ada933b3a1954
sneekjs <sneekjs@users.noreply.github.com>
2018-03-02 12:03:25 +0100

    added a lot to world manager and there now spawn rocks
    rock solid

---
+++ Assets/Plugins/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float cameraSpeed = 10f;
+    public float zoomSpeed = 7f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 newPos = transform.position;
+        newPos.x += Time.deltaTime * x * cameraSpeed;
+        newPos.z += Time.deltaTime * y * cameraSpeed;
+
+        transform.position = newPos;
+
+        if (Input.GetKey(KeyCode.Z))
+            Zoom(zoomSpeed);
+        else if (Input.GetKey(KeyCode.X))
+            Zoom(-zoomSpeed);
+    }
+
+    private void Zoom(float z)
+    {
+        Vector3 newPos = transform.position;
+        newPos.z += Time.deltaTime * z;
+        newPos.z = Mathf.Clamp(newPos.z, -50f, -10f);
+        transform.position = newPos;
+    }
+}
