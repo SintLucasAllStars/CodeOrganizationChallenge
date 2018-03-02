18e9e42f744d387f73c7694934e6c0d2eecff658
Bulletdrone <Sam>
2018-03-02 12:07:09 +0100

    added prefabs, changed your shitty texture on the world, added 3 colours that divide the beasts aggressiveness. Added variables to Animals.

--- Assets/Classes/SClass/Animals.cs
+++ Assets/Classes/SClass/Animals.cs
@@ -6,7 +6,13 @@ public class Animals : Creatures {
 	float instinct;
 
 	public Animals (EatingBehaviour eB, float sz, float fer, float at, float ma, float a) { //attempt at animal template. sz size, fer fertility, at attractionRate, ma maxAge, a Aggression
+		sz = size;
+		fer = fertility;
+		at = attractionRate;
+		ma = maxAge;
+		a = aggression;
 		GetComponent<SphereCollider> ().radius = combatEfficiency / 2 + aggression; //sets the detection radius
-
+		combatEfficiency = size + a;
+		speed = 1 / size;
 	}
 }
