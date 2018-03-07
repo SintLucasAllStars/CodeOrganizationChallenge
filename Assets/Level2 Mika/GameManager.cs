4c8c95b5d250c82d4e28ae54fb7804ba4202fb73
MikavanderZanden <mikavanderzanden@gmail.com>
2018-03-07 10:19:08 +0100

    finished level 2

---
+++ Assets/Level2 Mika/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public static GameManager ins;
+	public Node currentNode;
+
+	void Awake ()
+	{
+	
+		ins = this;
+	
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
--- Assets/Level2 Mika/Node.cs
+++ Assets/Level2 Mika/Node.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public abstract class Node : MonoBehaviour
 {
 
 	public Transform camPos;
 	public List<Node> reachNodes = new List<Node> ();
+
 
 	[HideInInspector]
 	public Collider col;
@@ -15,5 +17,45 @@ public abstract class Node : MonoBehaviour
 	{
 		col = GetComponent<Collider> ();
 		
+	}
+
+	void OnMouseDown (){
+		Debug.Log ("Node clicked");
+
+		Arrive ();
+
+	
+	}
+
+	void Arrive(){
+		if (GameManager.ins.currentNode != null) {
+			GameManager.ins.currentNode.Leave ();
+		}
+
+		GameManager.ins.currentNode = this;
+
+		Camera.main.transform.position = camPos.position;
+		Camera.main.transform.rotation = camPos.rotation;
+
+		if (col != null) {
+			col.enabled = false;
+		}
+
+		foreach (Node node in reachNodes) {
+			if (node.col != null) {
+				node.col.enabled = true;
+			}
+
+		}
+
+	}
+
+	public void Leave(){
+		foreach (Node node in reachNodes) {
+			if (node.col != null) {
+				node.col.enabled = false;
+			}
+
+		}
 	}
 }
