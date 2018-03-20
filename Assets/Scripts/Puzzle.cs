8058ae58c19ce72b564510ce477c295e39ef8ed1
mikedriessen04@gmail.com <36692429+mikedriessen@users.noreply.github.com>
2018-03-20 22:33:45 +0100

    Simple escape room thingy, not done

---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle {
+
+	public string question;
+	public string answer;
+
+	public Puzzle(string question, string answer)
+	{
+		this.question = question;
+		this.answer = answer;
+	}
+}
