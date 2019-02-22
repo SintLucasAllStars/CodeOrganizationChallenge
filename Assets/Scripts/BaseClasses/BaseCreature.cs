5bb59c019f4fa3969b14bc26a56f1a1b91b20844
Quinn McGregor <quinn1115@users.noreply.github.com>
2019-02-22 12:29:32 +0100

    #2 Walking AI

---
+++ Assets/Scripts/BaseClasses/BaseCreature.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class BaseCreature : MonoBehaviour
+{
+    //AI
+    NavMeshAgent navAgent;
+    public DNA Genes = new DNA();
+    public GenderType gender;
+    public FoodType foodType;
+    public GameObject Mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        Genes.Gender = gender;
+        Genes.foodType = foodType;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wander(15);         
+    }
+
+    void Wander(float maxDistance)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * maxDistance;
+        NavMeshHit hit;
+
+        if (navAgent)
+        {
+            if(transform.position == navAgent.destination)
+            {
+                randomDirection += transform.position;
+                NavMesh.SamplePosition(randomDirection, out hit, maxDistance, 1);
+                navAgent.destination = hit.position;
+
+            }
+        }
+    }
+
+ 
+    
+}
---
+++ Assets/Scripts/BaseClasses/DNA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GenderType
+{
+    Male,
+    Female,
+}
+
+public enum FoodType
+{
+    Carnivore,
+    Herbivore,
+    Omnivore,
+
+}
+
+
+public class DNA
+{
+    //Variables
+    public float speed;
+    public float Size;
+    public float Strength;
+    public float Aggresion;
+    public GenderType Gender;
+    public FoodType foodType;
+    public Color skinColor;
+
+
+
+    public DNA()
+    {
+    
+    }
+
+
+}
