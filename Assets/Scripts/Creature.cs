7edc283b7816102311e875e945c4afb1b14c844f
KKEV1NN <44778147+KKEV1NN@users.noreply.github.com>
2019-02-26 20:25:11 +0100

    Commit
    Removed genes

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -2,14 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Creature : MonoBehaviour
+public class Creature : MonoBehaviour 
 {
 
-
+	DNA m_Dna; 
 
 	public Creature()
 	{
 
 	}
+
+	public void Start()
+	{
+		
+	}
+
+	public void Eat()
+	{
+
+	}
+
+	public void Walk()
+	{
+
+	}
+
+	public void Fight()
+	{
+
+	}
+
+	public void See()
+	{
+
+	}
+
+	public void Mate(DNA.Attributes parentA, DNA.Attributes parentB)
+	{
+
+
+
+	}
+
+
+
+
+
 
 }
--- Assets/Scripts/DNA.cs
+++ Assets/Scripts/DNA.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class DNA : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[System.Serializable]
+	public struct Attributes
+	{
+		public enum food
+		{
+			herbivore, omnivore, carnivore
+		};
+
+
+		public double m_Strength;
+
+		public double m_Speed;
+
+		public double m_Longevity;
+
+		public double m_Endurance;
+
+		public double m_Size;
+
+
+
+	}
+
+	Attributes Gene;
+
+	public DNA(Attributes parentA, Attributes parentB)
+	{
+
+		int m_Evolution = Random.Range(1, 100);
+
+		if (m_Evolution >1)
+		{
+			Gene.m_Strength = 5; 
+		}
+
+
+	}
+
+
+
 }
