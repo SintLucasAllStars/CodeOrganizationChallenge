3a696477aa5e54c48760f800fd935f1356a49fc0
KKEV1NN <44778147+KKEV1NN@users.noreply.github.com>
2019-03-01 21:31:28 +0100

    Commit
    Replaced Creature.

---
+++ Assets/Scripts/Critter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Critter : MonoBehaviour
+{
+
+	NavMeshAgent navAgent; 
+
+	public Critter()
+	{
+
+	}
+
+	public void Start()
+	{
+		navAgent = GetComponent<NavMeshAgent>(); 
+	}
+
+	private void Update()
+	{
+		Idle(15);
+	}
+
+	public void Idle(float maxDistance)
+	{
+		//sets a random direction
+		Vector3 randomDirection = Random.insideUnitSphere * maxDistance;
+		if (navAgent != null)
+		{
+			
+		
+			if (Vector3.Distance(transform.position, navAgent.destination) < 0.5f)
+			{
+				
+				randomDirection += transform.position;
+				NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, maxDistance, 1);
+				navAgent.destination = hit.position;
+
+			}
+		}
+	}
+
+
+}	
--- Assets/Scripts/DNA.cs
+++ Assets/Scripts/DNA.cs
@@ -5,30 +5,8 @@ using UnityEngine;
 public class DNA : MonoBehaviour
 {
 
-	[System.Serializable]
-	public struct Attributes
-	{
-		public enum food
-		{
-			herbivore, omnivore, carnivore
-		};
 
-
-		public double m_Strength;
-
-		public double m_Speed;
-
-		public double m_Longevity;
-
-		public double m_Endurance;
-
-		public double m_Size;
-
-
-
-	}
-
-	Attributes Gene;
+	public Attributes Gene; 
 
 	public DNA(Attributes parentA, Attributes parentB)
 	{
@@ -42,6 +20,82 @@ public class DNA : MonoBehaviour
 
 
 	}
+
+	public void Breed(Attributes parentA, Attributes parentB)
+	{
+
+		int m_Evolution = Random.Range(1, 6);
+
+		if (m_Evolution == 1)
+		{
+			Gene.m_Strength = Random.Range(1, 100);
+			Gene.m_Speed = Random.Range(1, 100);
+			Gene.m_Longevity = Random.Range(1, 100);
+			Gene.m_Endurance = Random.Range(1, 100);
+			Gene.m_Size = Random.Range(1, 100);
+			Gene.isMale = true;
+			Gene.m_ID = parentA.m_ID;
+
+		}
+		else if (m_Evolution == 2)
+		{
+			Gene.m_Strength = Random.Range(1, 100);
+			Gene.m_Speed = Random.Range(1, 100);
+			Gene.m_Longevity = Random.Range(1, 100);
+			Gene.m_Endurance = Random.Range(1, 100);
+			Gene.m_Size = Random.Range(1, 100);
+			Gene.isMale = false;
+			Gene.m_ID = parentA.m_ID;
+
+		}
+		else if (m_Evolution == 3)
+		{
+			Gene = parentB;
+			Gene.m_ID = parentA.m_ID;
+
+		}
+		else if (m_Evolution == 4)
+		{
+			Gene = parentA;
+			Gene.m_ID = parentA.m_ID;
+
+		}
+		else if (m_Evolution == 5)
+		{
+			Gene = parentA;
+			Gene.m_ID = parentA.m_ID;
+
+		}
+
+
+	}
+
+
+
+}
+
+[System.Serializable]
+public struct Attributes
+{
+	public enum Food
+	{
+		herbivore, omnivore, carnivore
+
+	};
+
+	public int m_ID; 
+
+	public bool isMale; 
+
+	public double m_Strength;
+
+	public double m_Speed;
+
+	public double m_Longevity;
+
+	public double m_Endurance;
+
+	public double m_Size;
 
 
 
