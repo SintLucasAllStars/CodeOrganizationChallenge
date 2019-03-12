461e1eac815a4d5dc94e5786734fbeee1a5fea7c
SanneWouters <47358168+SanneWouters@users.noreply.github.com>
2019-03-12 14:13:14 +0100

    The whole thing in one commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+
+    public static Manager Instance;
+
+
+    List<Creature> creatures;
+    public GameObject prefab;
+    public int initCreatures;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    private void Start()
+    {
+        creatures = new List<Creature>();
+        for (int i = 0; i < initCreatures; i++)
+        {
+            creatures.Add(new Creature(prefab, Random.Range(-20, 20), 0, Random.Range(-20, 20)));
+        }
+    }
+
+
+
+    private void Update()
+    {
+        foreach (Creature c in creatures)
+        {
+            Vector3 ganaar = new Vector3(Random.Range(-1000, 1000), 0, Random.Range(-1000, 1000));
+            c.Move(ganaar);
+        }
+    }
+
+
+    public void RegisterCreature(Transform transform)
+    {
+        creatures.Add(new Creature(prefab, transform.position.x, transform.position.y, transform.position.z));
+    }
+
+   
+
+
+}
+
